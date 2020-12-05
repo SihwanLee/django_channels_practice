a3e953ade9bae2294c2f80dfaadb9e7f218e332a
Sihwan Lee <alexxlee66@gmail.com>
2020-12-06 00:02:13 +0900

    Tutorial Part 2: Implement a Chat Server
    Added the room view
    Writed the first consumer
    Enabled a channel layer
    Download Redis 5.0.10 for Windows
    Created redis-windows-read.txt

--- webchat/webchat/settings.py
+++ webchat/webchat/settings.py
@@ -71,7 +71,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webchat.wsgi.application'
 ASGI_APPLICATION = 'webchat.asgi.application'
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
